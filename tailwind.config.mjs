e153e6a76505ab2b0ecdbe2b47d8763a4be333fe
emman355 <earevalo355@gmail.com>
2025-11-18 21:35:39 +0800

    initial Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,64 @@
+// tailwind.config.mjs
+
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/react/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--color-background)",
+        foreground: "var(--color-foreground)",
+
+        border: "var(--color-border)",
+        ring: "var(--color-ring)",
+
+        primary: {
+          DEFAULT: "var(--color-primary)",
+          foreground: "var(--color-primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "var(--color-secondary)",
+          foreground: "var(--color-secondary-foreground)",
+        },
+        accent: {
+          DEFAULT: "var(--color-accent)",
+          foreground: "var(--color-accent-foreground)",
+        },
+        destructive: {
+          DEFAULT: "var(--color-destructive)",
+          foreground: "var(--color-destructive-foreground)",
+        },
+
+        muted: {
+          DEFAULT: "var(--color-muted)",
+          foreground: "var(--color-muted-foreground)",
+        },
+        card: {
+          DEFAULT: "var(--color-card)",
+          foreground: "var(--color-card-foreground)",
+        },
+        popover: {
+          DEFAULT: "var(--color-popover)",
+          foreground: "var(--color-popover-foreground)",
+        },
+        sidebar: {
+          DEFAULT: "var(--color-sidebar)",
+          foreground: "var(--color-sidebar-foreground)",
+          primary: "var(--color-sidebar-primary)",
+          "primary-foreground": "var(--color-sidebar-primary-foreground)",
+          accent: "var(--color-sidebar-accent)",
+          "accent-foreground": "var(--color-sidebar-accent-foreground)",
+          border: "var(--color-sidebar-border)",
+          ring: "var(--color-sidebar-ring)",
+        },
+      }
+      
+    },
+  },
+  plugins: [],
+};
+
+export default config;
